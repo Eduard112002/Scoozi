f4878b7399d5feab87ddd5530c9383dd5c4304a1
Eduard112002 <123071598+Eduard112002@users.noreply.github.com>
2024-07-27 02:20:19 +0300

    Доработка формы. Добавил Ant Design.

--- src/components/form-booking/form-booking.js
+++ src/components/form-booking/form-booking.js
@@ -3,11 +3,29 @@ import './form-booking.css';
 import { useDispatch } from 'react-redux';
 import {useSelector} from "react-redux";
 
-import { exitFormBooking } from "../../actions";
+import { DatePicker, Space } from 'antd';
+import ru from 'antd/es/date-picker/locale/ru_RU';
+import { exitFormBooking, numberPeople } from "../../actions";
 
+const buddhistLocale= {
+    ...ru,
+    lang: {
+        ...ru.lang,
+        fieldDateFormat: 'YYYY-MM-DD',
+        fieldDateTimeFormat: 'YYYY-MM-DD HH:mm:ss',
+        yearFormat: 'YYYY',
+        cellYearFormat: 'YYYY',
+    },
+};
 
 function FormBooking({}) {
     const dispatch = useDispatch();
+    const numberPeopleList = ['Количество человек *','1 человек', '2 человека', '3 человека', '4 человека', '5 человек',
+        '6 человек', '7 человек', '8 человек', '9 человек', '10 человек', 'Больше 10 человек']
+        .map((el, index) => <option key={index} value={index}>{el}</option>);
+    const handleChange = (e) => {
+         dispatch(numberPeople(e.target.value))
+    };
     const displayForm = useSelector((s) => s.displayForm);
     function exitForm () {
         dispatch(exitFormBooking(false));
@@ -29,10 +47,15 @@ function FormBooking({}) {
                     <input className="info_user" placeholder="Ваше имя *" type="text" maxLength="30"/>
                     <input className="info_user" placeholder="Ваш телефон *" type="tel"/>
                     <input className="info_user" placeholder="Ваш E-mail" type="email"/>
-                    <input className="info_user" placeholder="Количество человек *" type="text"/>
+                    <select  className="form_people info_user" onChange={handleChange}>
+                        {numberPeopleList}
+                    </select>
                 </div>
-                <div>
-                    <input/>
+                <div className="form_time">
+                    <label className="form_time_title">Укажите дату и время *</label>
+                    <Space direction="vertical" >
+                        <DatePicker locale={buddhistLocale}/>
+                    </Space>
                 </div>
             </form>
         </div>
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,6 @@
-import React, {useState} from "react";
+import React from "react";
 import './header.css';
-import {useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 
 import { openFormBooking } from "../../actions";
 
